e65afe6bc8fd1e6956ae174cc8308ec9028e94ff
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-09-23 14:13:19 -0700

    practice with cookies and sessions in php

--- index.php
+++ index.php
@@ -5,14 +5,24 @@
     <title>Mock Title</title>
 </head>
 <body>
+ <!-- <?php
+     echo $_GET["name"];
+   ?> -->
 
-  <?php
-    echo $_GET["name"];
-  ?>
-
-  <form method="GET">
+  <!-- <form method="GET">
     <input type="hidden" name="name" value="Belac">
     <button type="submit">Submit</button>
-  </form>
+  </form> -->
+
+  <!-- will destroy cookie right away -->
+  setcookie("name", "Belac", time() - 1);
+
+  <!-- will destroy cookie in a day -->
+  setcookie("name", "Belac", time() + 86400);
+  
+  <!-- Session variable is more safe against hackers, sessions end on close -->
+  $_SESSION["name"] = "12";
+
+
 </body>
 </html>
